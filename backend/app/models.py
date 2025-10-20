e8025e1ad04d77fb056acbd31d9f97a2d78cd801
KalilovM <mn.kalilov@gmail.com>
2025-10-21 00:05:10 +0200

    adding models to backend and isort pre commit

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,16 @@
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String, text
 import uuid
+from datetime import UTC, datetime
+
 from database import Base
+from sqlalchemy import Boolean, DateTime, Enum, String, text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 
-# class Role(StrEnum)
+class Role(Enum):
+    # TODO: Think about the roles in the application
+    ADMIN = "ADMIN"
+    USER = "USER"
 
 
 class User(Base):
@@ -18,7 +23,20 @@ class User(Base):
         server_default=text("gen_random_uuid()"),
         init=False,
     )
+    # TODO: ask about the id field, (e.g default_factory, server_default)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(20), nullable=False)
     salt: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[Role] = mapped_column(
+        Enum(Role), default=Role.USER, server_default=text(f"'{Role.USER.value}")
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=datetime.now(UTC)
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=datetime.now(UTC)
+    )
+
+    # TODO: add alembic
